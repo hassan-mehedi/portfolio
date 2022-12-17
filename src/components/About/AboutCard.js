1e53283d9598f3d99bd5159340d0d47e6419c9ff
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-12-17 21:06:27 +0600

    Edit about section

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -3,39 +3,36 @@ import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
-  return (
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+    return (
+        <Card className="quote-card-view">
+            <Card.Body>
+                <blockquote className="blockquote mb-0">
+                    <p style={{ textAlign: "justify" }}>
+                        Hi Everyone, I am <span className="purple">Md. Mehedi Hassan </span>
+                        from <span className="purple"> Dhaka, Bangladesh.</span>
+                        <br />I am a Junior Software Engineer.
+                        <br />
+                        <br />
+                        Apart from coding, some other activities that I love to do!
+                    </p>
+                    <ul>
+                        <li className="about-activity">
+                            <ImPointRight /> Playing Games
+                        </li>
+                        <li className="about-activity">
+                            <ImPointRight /> Reading Novels, Manga & Tech Articles
+                        </li>
+                        <li className="about-activity">
+                            <ImPointRight /> Tasting New Foods
+                        </li>
+                    </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
-  );
+                    <p style={{ color: "rgb(155 126 172)" }}>"The harder I work, the luckier I get." </p>
+                    <footer className="blockquote-footer">Samuel Goldwyn</footer>
+                </blockquote>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default AboutCard;
--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,55 +1,43 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-} from "react-icons/di";
-import {
-  SiPytorch,
-  SiFirebase,
-  SiNextdotjs,
-} from "react-icons/si";
+import { DiJavascript1, DiReact, DiNodejs, DiMongodb, DiPython, DiGit, DiPostgresql, DiDocker, DiNpm, DiTerminal } from "react-icons/di";
+import { SiAmazonaws, SiGnubash } from "react-icons/si";
 
 function Techstack() {
-  return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPytorch />
-      </Col>
-    </Row>
-  );
+    return (
+        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiReact />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiPostgresql />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiNodejs />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiMongodb />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiNpm />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiGit />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiAmazonaws />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiGnubash />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiJavascript1 />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiPython />
+            </Col>
+        </Row>
+    );
 }
 
 export default Techstack;
