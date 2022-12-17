82cc59cd0a92383f21513be12c386e571f68ff71
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-12-17 21:13:43 +0600

    edit home section

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -2,21 +2,16 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+    return (
+        <Typewriter
+            options={{
+                strings: ["Developer", "Junior Software Engineer", "MERN & PERN Stack Developer"],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+            }}
+        />
+    );
 }
 
 export default Type;
